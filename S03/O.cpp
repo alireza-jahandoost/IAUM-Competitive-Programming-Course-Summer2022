14bc0ec4b33a68320266c1431418e583dbf55cdf
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-07-27 12:05:58 +0430

    Finish session 03

---
+++ S03/O.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	// O(1)
+	int a;
+	a = 10;
+	cin>>a;
+
+	// O(n)
+	for(int i=0;i<n;i++)
+		// do something
+	for(int i=4;i<n*7;i++)
+		// do something
+	for(int i=0;i<n;i+=7)
+		// do something
+
+	// O(n^2)
+	for(int i=0;i<n;i++)
+		for(int j=4;j<n/2;j++)
+			// do something
+	
+	// O(n.m)
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			// do something
+
+	// O(log n)
+	for(int i=0;i<n;i*=2)
+		// do something
+	for(int i=n*4;i>=0;i/=2)
+		// do something
+	
+	// O(n!)
+	// permutations of an array
+
+	// O(2^n)
+	// number of subsets of an array
+	
+	return 0;
+}
